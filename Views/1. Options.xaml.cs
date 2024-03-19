3044b11173457e4b746c8ebf97f8e53c80341ce6
backslashspace <86926768+backslashspace@users.noreply.github.com>
2024-03-19 13:28:18 +0100

    added reactive option page

--- Views/1. Options.xaml.cs
+++ Views/1. Options.xaml.cs
@@ -8,13 +8,68 @@ namespace ConfigGen.Views
         public Options()
         {
             InitializeComponent();
-
-            Loaded += OnLoaded;
         }
 
-        private void OnLoaded(object sender, EventArgs e)
+        private void ActivateUserInputField(object sender, System.Windows.RoutedEventArgs e)
         {
-            Test.Content = $"{TheGrid.ActualHeight}, {TheGrid.ActualWidth}";
+            CheckBox checkBox = sender as CheckBox;
+
+            String tag;
+            Boolean state;
+
+            try
+            {
+                tag = (String)checkBox.Tag;
+                state = (Boolean)checkBox.IsChecked;
+            }
+            catch
+            {
+                return;
+            }
+
+            switch (tag)
+            {
+                case "PrivateKeyBox":
+                    UseUserServerPrivateKeyValue.IsEnabled = state;
+                    break;
+
+                case "ServerKeepAlive":
+                    ServerKeepAliveValue.IsEnabled = state;
+                    break;
+
+                case "ClientKeepAlive":
+                    ClientKeepAliveValue.IsEnabled = state;
+                    break;
+            }
+        }
+
+
+        private void TextBox_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            try
+            {
+                if ((String)textBox.Tag == "ignore")
+                {
+                    return;
+                }
+            }
+            catch { }
+
+            Boolean state = false;
+
+            try
+            {
+                state = (Boolean)textBox.Tag;
+            }
+            catch { }
+
+            if (!state)
+            {
+                textBox.Text = "";
+                textBox.Tag = true;
+            }
         }
     }
 }
